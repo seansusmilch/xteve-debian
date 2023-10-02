ef330e8f72fdace505758b4d3ebfa67010107e2b
Sean <seantsusmilch@gmail.com>
2023-10-02 01:47:47 -0500

    Changing repo and container name

--- buildnrun.cmd
+++ buildnrun.cmd
@@ -1,5 +1,5 @@
-docker build . -t superminecraftkid64/xteve-debian
-docker rm -f xteve-debian
+docker build . -t superminecraftkid64/xteve-streamlink
+docker rm -f xteve-streamlink
 
-@REM docker run -p 34400:34400 --name xteve-debian -v .\mnt\config:/config -v .\mnt\root:/root/.xteve -v .\mnt\tmp:/tmp/xteve -e TZ='America/Chicago' superminecraftkid64/xteve-debian
+@REM docker run -p 34400:34400 --name xteve-streamlink -v .\mnt\config:/config -v .\mnt\root:/root/.xteve -v .\mnt\tmp:/tmp/xteve -e TZ='America/Chicago' superminecraftkid64/xteve-streamlink
 docker compose up
